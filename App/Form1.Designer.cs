36355728dc82d5820d44425e5af4c990c77d76d0
Filipe Ferreira <lipe.ferreira1609@hotmail.com>
2020-05-17 00:28:53 -0300

    first

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BuscaCep
+{
+    partial class BuscaCepApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BuscaCepApp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_cep = new System.Windows.Forms.TextBox();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Busca endereço via CEP";
+            // 
+            // txt_cep
+            // 
+            this.txt_cep.Location = new System.Drawing.Point(43, 56);
+            this.txt_cep.Name = "txt_cep";
+            this.txt_cep.Size = new System.Drawing.Size(250, 20);
+            this.txt_cep.TabIndex = 1;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(299, 53);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(53, 23);
+            this.btn_buscar.TabIndex = 2;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Location = new System.Drawing.Point(43, 154);
+            this.txt_resultado.Multiline = true;
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.ReadOnly = true;
+            this.txt_resultado.Size = new System.Drawing.Size(309, 136);
+            this.txt_resultado.TabIndex = 3;
+            // 
+            // BuscaCepApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(394, 381);
+            this.Controls.Add(this.txt_resultado);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.txt_cep);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BuscaCepApp";
+            this.Opacity = 0.85D;
+            this.Text = "Cep Fácil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_cep;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.TextBox txt_resultado;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,34 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaCep
+{
+    public partial class BuscaCepApp : Form
+    {
+        public BuscaCepApp()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            CepService cepService = new CepService();
+            string enderecoFormatado="";
+
+            var endereco = cepService.buscarEndereco(txt_cep.Text);
+            if(!string.IsNullOrEmpty(endereco.cep))
+                enderecoFormatado = $"Rua: {endereco.logradouro},\r\nBairro: {endereco.bairro},\r\nEstado: {endereco.uf},\r\nCep: {endereco.cep}";
+
+            txt_resultado.Text = enderecoFormatado;
+            txt_cep.Clear();
+        }
+    }
+}
